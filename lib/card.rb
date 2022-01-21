09f028d7c0b39787202fe597964f9d6d42aa7024
Robert Neal <robertwneal2@gmail.com>
2022-01-21 08:11:01 -0800

    added @suit to card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,10 @@
 class Card
 
-    attr_reader :val
+    attr_reader :val, :suit
 
-    def initialize(val)
+    def initialize(val, suit)
         @val = val
+        @suit = suit
     end
     
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,10 +3,11 @@ require 'card'
 
 describe Card do
 
-    subject(:card) { Card.new("j")}
+    subject(:card) { Card.new("j", "c")}
 
-    it "has a readable value" do
+    it "has a readable value and suit" do
         expect(card.val).to eq("j")
+        expect(card.suit).to eq("c")
     end
 
 end
