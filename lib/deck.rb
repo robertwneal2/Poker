1514805181a3fb64e12db1857e76d46a11afe62c
Robert Neal <robertwneal2@gmail.com>
2022-01-21 08:37:34 -0800

    deck initiation and spec complete

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,26 @@
+require 'byebug'
+require_relative 'card'
+
 class Deck
+
+    CARD_VALUES = ["2", "3", "4", "5", "6", "7", "8",
+                   "9", "10", "J", "Q", "K", "A"]
+
+    CARD_SUITS = ["H", "D", "S", "C"]
+
+    attr_reader :cards
+    def initialize
+        @cards = []
+        CARD_VALUES.each do |val|
+            CARD_SUITS.each do |suit|
+                @cards << Card.new(val, suit)
+            end
+        end
+        shuffle
+    end
+
+    def shuffle
+        @cards = @cards.shuffle
+    end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,41 @@
 require 'rspec'
 require 'deck'
+
+describe Deck do
+
+    subject(:deck) { Deck.new }
+
+    it "deck initializes with a 'cards' variable of length 52" do
+        expect(deck.cards.length).to eq(52)
+    end
+
+    it "deck contains Card instances" do
+        expect(deck.cards[0].class).to eq(Card)
+    end
+
+    it "contains 13 different card values, 4 suits each" do
+        expect(deck.cards.count { |card| card.val == "J"}).to eq(4)
+        expect(deck.cards.uniq{ |card| card.val}.count).to eq(13)
+        expect(deck.cards.uniq{ |card| card.suit}.count).to eq(4)
+    end
+
+    it "shuffles the deck on creation" do
+        values = ["2", "3", "4", "5", "6", "7", "8",
+                  "9", "10", "J", "Q", "K", "A"]
+        suits = ["H", "D", "S", "C"]
+        unshuffled_cards = []
+        values.each do |val|
+            suits.each do |suit|
+                unshuffled_cards << [val, suit]
+            end
+        end
+        shuffled_cards = []
+        deck.cards.each do |card|
+            val = card.val
+            suit = card.suit
+            shuffled_cards << [val, suit]
+        end
+        expect(shuffled_cards).to_not eq(unshuffled_cards)
+    end
+
+end
