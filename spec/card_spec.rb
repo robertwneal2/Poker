2ad9f2be56b0acbd9dffa1cb43c9eba5fcb3d3b7
Robert Neal <robertwneal2@gmail.com>
2022-01-21 08:13:17 -0800

    fixed card inputs again

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -6,8 +6,8 @@ describe Card do
     subject(:card) { Card.new("J", "C")}
 
     it "has a readable value and suit" do
-        expect(card.val).to eq("j")
-        expect(card.suit).to eq("c")
+        expect(card.val).to eq("J")
+        expect(card.suit).to eq("C")
     end
 
 end
